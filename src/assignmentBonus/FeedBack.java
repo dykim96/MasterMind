7530eaeb044ae5c7a05fdcccdd85c00aeb5ac054
doasi_000 <doasi_000@Lenovo-PC>
2016-04-24 15:30:43 -0500

    FeedBack ver 2

--- src/assignmentBonus/FeedBack.java
+++ src/assignmentBonus/FeedBack.java
@@ -8,13 +8,64 @@
  */
 
 package assignmentBonus;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+
 
 public class FeedBack
 {
-
-	public FeedBack()
+	int num_Peg;
+	Rectangle Frame;
+	//Ellipse2D.Double Dots;
+	ArrayList<Ellipse2D.Double> Dots;
+	Color[] answer;
+	Color[] guess;
+	int color_match;
+	int all_match;
+		// input guess array, input array)
+		// output void , but should have draw method that will used by the main
+		// 
+	public FeedBack(int x, int y, int num, Color[] answer, Color[] guess)
 	{
-		// TODO Auto-generated constructor stub
+		this.Frame = new Rectangle(x, y, 36, 36); 
+		num_Peg = num;
+		Dots = new ArrayList<Ellipse2D.Double>();
+		this.answer = answer;
+		this.guess = guess;
+		color_match = 0;
+		all_match = 0;
+//		for (int i = 0 ;  i < num ; i ++){
+//			
+//			Ellipse2D.Double Dot = new Ellipse2D.Double
+//			Dots.add()
+//			
+//		}
+		
 	}
-
+	
+	public void Draw(){
+		
+		
+		
+		
+		
+	}
+	
+	public void ans_chk(){
+		for (int i= 0; i < num_Peg ; i++){
+			if (answer[0].equals(guess[i])){
+				all_match += 1;
+			}
+			
+			
+		}
+		
+		
+		
+	}
+	
 }
