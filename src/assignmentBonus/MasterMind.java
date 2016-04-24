0b87210148a21e4f645cdc02c42a567bc9b27776
dykim96 <toueikin@gmail.com>
2016-04-24 14:01:55 -0500

    made basic structure for the game board

--- src/assignmentBonus/MasterMind.java
+++ src/assignmentBonus/MasterMind.java
@@ -11,11 +11,15 @@ package assignmentBonus;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.JComboBox;
 
 public class MasterMind extends Applet implements MouseListener, MouseMotionListener{
 	
 	private int mouseX, mouseY;
+	private int guessNumber, pegNumber;
 	public void init(){
+		guessNumber = 12;
+		pegNumber = 4;
 		mouseX = 0;
 		mouseY = 0;
 		addMouseListener(this);
@@ -43,6 +47,18 @@ public class MasterMind extends Applet implements MouseListener, MouseMotionList
 	}
 	
 	public void paint(Graphics g) {
+		//max size Width 216, Height 504 (217, 505 as parameter)
+		//up to 6pegs, up to 12 guesses
+		//maybe make each rows into a separate class
+		for(int i = 1; i < 14; i++){
+		    g.drawRect(0, 36*i, 36*6, 36);
+		    g.drawOval(3, 3 + 36*i, 30, 30);
+		    g.drawOval(3 + 36, 3 + 36*i, 30, 30);
+		    g.drawOval(3 + 72, 3 + 36*i, 30, 30);
+		    g.drawOval(3 + 108, 3 + 36*i, 30, 30);
+		    g.drawOval(3 + 144, 3 + 36*i, 30, 30);
+		    g.drawOval(3 + 180, 3 + 36*i, 30, 30);
+	    }
 	    g.drawString("(" + mouseX + "," + mouseY + ")", 0, 10);
 	}
 }
