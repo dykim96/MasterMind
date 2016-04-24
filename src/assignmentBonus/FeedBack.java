3cd4d961e8bef162ab8c5c989d08ab6f12b380af
doasi_000 <doasi_000@Lenovo-PC>
2016-04-24 15:08:28 -0500

    feedback start

---
+++ src/assignmentBonus/FeedBack.java
@@ -0,0 +1,20 @@
+/*  Assignment Bonus: Master Mind
+ *  Create game, Master Mind, with Java
+ *  Section: 16185
+ *  Name1: Doyoung Kim
+ *  UTEID1: dk24338
+ *  Name2: Hyo-Jung Kim
+ *  UTEID2: hk6336
+ */
+
+package assignmentBonus;
+
+public class FeedBack
+{
+
+	public FeedBack()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
